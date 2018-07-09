e9f95c47f310c976f93d2b7a74c8d0bde556e0d6
Matt <matthew.wertheimer@successacademies.org>
2018-07-09 13:23:43 -0400

    v15

--- DEP-setup-new-user-local.sh
+++ DEP-setup-new-user-local.sh
@@ -8,15 +8,17 @@ user_name=`id -un $user_id`
 
 # open SplashBuddy app
 su $loggedInUser -c 'open -a /Library/Application Support/SplashBuddy/SplashBuddy.app'
+sleep 3
+su $loggedInUser -c 'open -a /Library/Application Support/SplashBuddy/SplashBuddy.app'
 
 # Setting ComputerName
 echo "==================================================================================" >> /var/log/jamf.log
-echo "Setting computer name to serial number..."
+echo "Setting computer name to serial number..." >> /var/log/jamf.log
 jamf setComputerName -name $system_serial
 echo "==================================================================================" >> /var/log/jamf.log
 
 echo "==================================================================================" >> /var/log/jamf.log
-echo "Creating sucadmin..."
+echo "Creating sucadmin..." >> /var/log/jamf.log
 jamf policy -event DEPMakeSucadmin
 echo "==================================================================================" >> /var/log/jamf.log
 
@@ -26,82 +28,82 @@ echo "==========================================================================
 # uptime reminder components), EnableRemoteMGMTDEP (enables ARD/SSH for sucadmin, PI_setfirmware
 # (sets firmware password), EnableLocation (enables location services), SetTimeServer
 # (sets 3 NTP time servers), sucadminprofile (sets sucadmin profile picture)
-echo "=================================================================================="
-echo "Running SA device customization policies..."
+echo "==================================================================================" >> /var/log/jamf.log
+echo "Running SA device customization policies..." >> /var/log/jamf.log
 jamf policy -event SystemSettings
-echo "=================================================================================="
+echo "==================================================================================" >> /var/log/jamf.log
 
 # Opening Safari to Okta for password reset
 jamf policy -event Okta
 
 # Finder FUT policies
-echo "=================================================================================="
-echo "Installing Finder FUT policies - will not be shown until restart..."
+echo "==================================================================================" >> /var/log/jamf.log
+echo "Installing Finder FUT policies - will not be shown until restart..." >> /var/log/jamf.log
 jamf policy -event FinderDEP
-echo "=================================================================================="
+echo "==================================================================================" >> /var/log/jamf.log
 
 # Installing Apple Enterprise Connect
-echo "=================================================================================="
-echo "Installing Apple Enterprise Connect..."
+echo "==================================================================================" >> /var/log/jamf.log
+echo "Installing Apple Enterprise Connect..." >> /var/log/jamf.log
 jamf policy -event EnterpriseConnect
-echo "=================================================================================="
+echo "==================================================================================" >> /var/log/jamf.log
 
 # Installing Apple Enterprise Connect
-echo "=================================================================================="
-echo "Checking if SonicWall installed from the App Store..."
+echo "==================================================================================" >> /var/log/jamf.log
+echo "Checking if SonicWall installed from the App Store..." >> /var/log/jamf.log
 if [ ! -d /Applications/SonicWall\ Mobile\ Connect.app ];
 	then echo "App Store failed. Installing from JSS..." && jamf policy -event SonicWall 
 	else
-		echo "Installing SonicWall_Mobile_Connect_VPP-v1.pkg"
-		echo "Actually, SonicWall was already installed."
-		echo "Sometimes things work correctly :) Skipping JSS policy..."
-		echo "Successfully installed SonicWall_Mobile_Connect_VPP-v1.pkg"
+		echo "Installing SonicWall_Mobile_Connect_VPP-v1.pkg" >> /var/log/jamf.log
+		echo "Actually, SonicWall was already installed." >> /var/log/jamf.log
+		echo "Sometimes things work correctly :) Skipping JSS policy..." >> /var/log/jamf.log
+		echo "Successfully installed SonicWall_Mobile_Connect_VPP-v1.pkg" >> /var/log/jamf.log
 	fi
-echo "=================================================================================="
+echo "==================================================================================" >> /var/log/jamf.log
 
 # Installing Google Chrome, enabling auto-updates, and setting as default browser
-echo "=================================================================================="
-echo "Installing current version of Google Chrome and setting as the default browser..."
+echo "==================================================================================" >> /var/log/jamf.log
+echo "Installing current version of Google Chrome and setting as the default browser..." >> /var/log/jamf.log
 jamf policy -event ChromeCurrent
 jamf policy -event ChromeDefault
-echo "=================================================================================="
+echo "==================================================================================" >> /var/log/jamf.log
 
 # Installing Flash and Java
-echo "=================================================================================="
-echo "Installing Java and Flash..."
+echo "==================================================================================" >> /var/log/jamf.log
+echo "Installing Java and Flash..." >> /var/log/jamf.log
 jamf policy -event BrowserPlugins
-echo "=================================================================================="
+echo "==================================================================================" >> /var/log/jamf.log
 
 # Installing Papercut Client
-echo "=================================================================================="
-echo "Installing Papercut Client..."
+echo "==================================================================================" >> /var/log/jamf.log
+echo "Installing Papercut Client..." >> /var/log/jamf.log
 jamf policy -event PCClient
-echo "=================================================================================="
+echo "==================================================================================" >> /var/log/jamf.log
 
 # Preparing Share Drive
-echo "=================================================================================="
-echo "Installing Shared Drive dependencies..."
+echo "==================================================================================" >> /var/log/jamf.log
+echo "Installing Shared Drive dependencies..." >> /var/log/jamf.log
 jamf policy -event SharedDrive
-echo "=================================================================================="
+echo "==================================================================================" >> /var/log/jamf.log
 
 # Installing BlueJeans
-echo "=================================================================================="
-echo "Installing BlueJeans Client..."
+echo "==================================================================================" >> /var/log/jamf.log
+echo "Installing BlueJeans Client..." >> /var/log/jamf.log
 jamf policy -event BlueJeans
-echo "=================================================================================="
+echo "==================================================================================" >> /var/log/jamf.log
 
 # Setting up user Dock
-echo "=================================================================================="
-echo "Configuring user Dock..."
+echo "==================================================================================" >> /var/log/jamf.log
+echo "Configuring user Dock..." >> /var/log/jamf.log
 jamf policy -event DEPDock
-echo "=================================================================================="
+echo "==================================================================================" >> /var/log/jamf.log
 
 # Creating 'Last Imaged' and 'Image Config' tokens
-echo "=================================================================================="
-echo "Verifying app installs and creating Staff High Sierra token and running recon..."
+echo "==================================================================================" >> /var/log/jamf.log
+echo "Verifying app installs and creating Staff High Sierra token and running recon..." >> /var/log/jamf.log
 sh /usr/local/bin/jss/DEP-install-verification.sh
 jamf policy -event DEPconfigstaffhighsierra
-echo "=================================================================================="
+echo "==================================================================================" >> /var/log/jamf.log
 
 # Quit SplashBuddy if still running
 if [[ $(pgrep SplashBuddy) ]]; then
@@ -109,6 +111,7 @@ if [[ $(pgrep SplashBuddy) ]]; then
 fi
 
 # we are done, so delete SplashBuddy + new user setup script
+echo "Deleting device setup files..." >> /var/log/jamf.log
 rm -rf '/Library/Application Support/SplashBuddy'
 rm /Library/Preferences/io.fti.SplashBuddy.plist
 rm /Library/LaunchAgents/io.fti.SplashBuddy.launch.plist
@@ -117,6 +120,7 @@ rm /usr/local/bin/jss/DEP-start-setup.sh
 rm /usr/local/bin/jss/DEP-install-verification.sh
 
 # Telling user device setup is complete
+echo "Device setup complete. Notifying user..." >> /var/log/jamf.log
 sh /usr/local/bin/jss/DEP-setup-complete.sh
 
 exit 0
