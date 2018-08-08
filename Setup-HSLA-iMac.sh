01af9d160818d1d0279e02c73f8d0a9240ab7601
Matt <matthew.wertheimer@successacademies.org>
2018-08-08 19:07:37 -0400

    Updated header

--- Setup-HSLA-iMac.sh
+++ Setup-HSLA-iMac.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#!/bin/bash
-
 # Define script variables
 system_serial=`system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'`
 loggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
 
+# Open Jamf Log so setup can be monitored
 su $loggedInUser -c 'open /var/log/jamf.log'
 
 # Setting ComputerName
