d7509297bb1e5e1943990f2cc568e6bfa1216963
Matt <matthew.wertheimer@successacademies.org>
2019-03-18 17:07:57 -0400

    Update install_bluejeans_current.sh
    initial commit

--- install_bluejeans_current.sh
+++ install_bluejeans_current.sh
@@ -4,23 +4,28 @@
 # script looks at BlueJeans large scale deployment site for the current mac installer, downloads and installs it
 # may need occasional validation if BlueJeans redesigns the site or URL nomenclature
 
-# Get the pkg download URL
+#  Get the pkg download URL
 installer_url=`/usr/bin/curl https://support.bluejeans.com/knowledge/app-large-deployment | grep mac/ |  cut -d \" -f 2`
 
 # Define location to save installer
 bluejeans_pkg="/tmp/BlueJeans.pkg"
 
 # Download installer
-/usr/bin/curl --output $bluejeans_pkg "$installer_url"
+/usr/bin/curl -k --silent --retry 3 --retry-max-time 6 --fail --output $bluejeans_pkg "$installer_url"
 
 # Install BlueJeans
 /usr/sbin/installer -pkg $bluejeans_pkg -target /
 
 # Confirm installation and set proper permissions
-if [ -d /Applications/Blue\ Jeans.app ]; then
+if [ -d /Applications/Blue\ Jeans.app ] ; then
+	bluejeans_version=`defaults read /Applications/Blue\ Jeans.app/Contents/Info CFBundleShortVersionString`
+	echo "BlueJeans "$bluejeans_version" installed successfully. Setting proper permissions..."
 	chown -R root:wheel /Applications/Blue\ Jeans.app/
 	chmod -R 775 /Applications/Blue\ Jeans.app/
-else exit 1
+	echo "Installation complete. Exiting..."
+else
+	echo "BlueJeans installation failed. Exiting.."
+	exit 1
 fi
 
 # Clean up
